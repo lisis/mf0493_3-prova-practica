f2d883cd921ebed03ce264a2df51c5f3d294b956
Carolina Lisa <carolina.lisa.ferrer@gmail.com>
2022-09-15 16:43:03 +0200

    3 unit test passed

--- unit-testing/isogram.test.js
+++ unit-testing/isogram.test.js
@@ -1,3 +1,20 @@
 import {it, expect, describe} from 'vitest';
 import { isIsogram } from './isogram';
 
+//1st test
+it ("should return true because 'Dermatoglyphics' is an Isogram", () => {
+    const result = isIsogram ("Dermatoglyphics");
+    expect (result).toBe(true); 
+});
+
+//2nd test
+it ("should return false because 'aba' contains repeated letters", () => {
+    const result = isIsogram ("aba");
+    expect (result).toBe(false); 
+});
+
+//3rd test
+it ("should return false because ignore letter case", () => {
+    const result = isIsogram ("moOse");
+    expect (result).toBe(false); 
+});
